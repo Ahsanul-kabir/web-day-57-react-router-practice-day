9dbc54becfbb6a19fef5dc1f47205550990a7ddd
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-15 21:02:16 +0600

    1 project setup and react router installation

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+  } from "react-router-dom";
+
+const Friend = (props) => {
+    const { name, email} = props.friend;
+    const friendStyle = {
+        border: '1px solid purple',
+        margin: '20px',
+        padding: '20px',
+        borderRadius: '20px'
+    }
+    return (
+        <div style={friendStyle}>
+            <h2>Name: {name}</h2>
+            <p>email: {email}</p>
+        </div>
+    );
+};
+
+export default Friend;
